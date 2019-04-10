6d25fff901f9efb54edc038f8069833463c4a8f1
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-10 08:43:20 +0500

    test(jest): fixed an issue where js files were not transformed

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const configs = require('@bluebase/code-standards/jest.config');
 
+const esModules = ['react-native', 'react-navigation-stack'].join('|');
+
 module.exports = Object.assign(configs, {
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"]  
 });
