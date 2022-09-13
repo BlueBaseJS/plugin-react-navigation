08dbe76062ca6e32755a215e862ba2f0bb5e0923
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-09-14 00:32:30 +0500

    feat: Linking prop is customizable via configs

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import { LoadingState, NavigationProps, NavigatorProps } from '@bluebase/components';
-import { useComponent, useConfig, useTheme } from '@bluebase/core';
-import { LinkingOptions, NavigationContainer } from '@react-navigation/native';
+import { useBlueBase, useComponent, useConfig, useTheme } from '@bluebase/core';
+import { getPathFromState, getStateFromPath, LinkingOptions, NavigationContainer } from '@react-navigation/native';
 import * as Linking from 'expo-linking';
 import React from 'react';
 
@@ -8,14 +8,21 @@ import { useBlueBaseContextPack } from '../../useBlueBaseContextPack';
 import { createLinkingConfigs } from './createLinkingConfigs';
 import { usePersistentState } from './usePersistentState';
 
+export type getStateFromPathConfigFn =
+(defaultState: any, path: string, options?: any) => any;
+
+export type getPathFromStateConfigFn =
+(defaultPath: string, state: any, options?: any) => any;
+
 /**
  * Navigation (V5)
  * This serves as an entry point where BlueBase passes routes and navigation
  * configs to this component.
  */
 export const Navigation = (props: NavigationProps) => {
-	const { navigator, ...rest } = props;
+	const BB = useBlueBase();
 	const { theme } = useTheme();
+	const { navigator, ...rest } = props;
 
 	const contextPack = useBlueBaseContextPack();
 	const Navigator = useComponent<NavigatorProps>('Navigator');
@@ -29,7 +36,23 @@ export const Navigation = (props: NavigationProps) => {
 
 	const linking: LinkingOptions<any> = {
 		config: createLinkingConfigs(navigator.routes, contextPack),
-		prefixes: [Linking.createURL('/'), ...prefixes]
+		prefixes: [Linking.createURL('/'), ...prefixes],
+		getStateFromPath: (path, options) => {
+			const getStateFromPathConfig = BB.Configs.getValue('navigation.linking.getStateFromPath');
+			const defaultState = getStateFromPath(path, options);
+
+			return getStateFromPathConfig
+				? getStateFromPathConfig(defaultState, path, options)
+				: defaultState;
+		},
+		getPathFromState: (state, options?) => {
+			const getPathFromStateConfig = BB.Configs.getValue('navigation.linking.getPathFromState');
+			const path = getPathFromState(state, options);
+
+			return getPathFromStateConfig
+				? getPathFromStateConfig(path, state, options)
+				: path;
+		},
 	};
 
 	return (
