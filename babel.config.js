acf7f60fe2398fd9e7269304de76837e8a813389
Abdul Rehman Talat <rehman.talat@gmail.com>
2024-04-01 23:04:22 +0500

    feat: Upgraded Expo 50

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,17 @@
 module.exports = function(api) {
 	api.cache(true);
 	return {
-		presets: ['babel-preset-expo', '@bluebase/code-standards/babel.config'],
-		plugins: ['react-native-reanimated/plugin'],
+		presets: ['@bluebase/code-standards/babel.config'],
+		plugins: [
+			'react-native-reanimated/plugin',
+			[
+				// https://github.com/facebook/metro/issues/646#issuecomment-799174473
+				'@babel/plugin-transform-react-jsx',
+				{
+					'runtime': 'automatic'
+				}
+			]
+		],
 		env: {
 			test: {
 				plugins: [
